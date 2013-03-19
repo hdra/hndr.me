db0cfda3513e9b998690d9d4c7be7728de0048db
Hendra <hendra2392@gmail.com>
2013-03-19 19:44:26 +0800

    initial commit

---
+++ configurations.py
@@ -0,0 +1,15 @@
+AUTHOR = u'Hendra'
+SITENAME = u"the dumpster"
+SITEURL = 'http://www.hndr.me'
+TIMEZONE = "Asia/Kuala_Lumpur"
+
+DEFAULT_CATEGORY = ('randoms')
+DEFAULT_DATE_FORMAT = ('%a %d %B %Y')
+DEFAULT_DATE = ('fs')
+
+# content path
+PATH = ('content/')
+# generated files path
+OUTPUT_PATH = ('output/')
+
+# THEME = 'themes/syte'
