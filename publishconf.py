3cd1f95d9f3fba3ef21d19e7c94d5cb6aea103e8
Hendra <hendra2392@gmail.com>
2018-05-03 08:42:36 +0700

    Update site url

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://hndr.me'
+SITEURL = 'https://hndr.me'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
